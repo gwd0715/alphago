184b5244b3237d85d6101a42f1daef55ff4e3cb1
Chris <gwd0715@gmail.com>
2020-05-03 06:02:44 +0800

    version 1.1 publish

---
+++ frontend/build/precache-manifest.dff677e825ea79948e7e07078618bd3c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b69e422a5a66d17cb74c2f2351c32ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2099cf1d380a8e242627",
+    "url": "/static/css/2.1445ab3f.chunk.css"
+  },
+  {
+    "revision": "04521365ddb7600954f8",
+    "url": "/static/css/main.1ad0d372.chunk.css"
+  },
+  {
+    "revision": "2099cf1d380a8e242627",
+    "url": "/static/js/2.b5928ce2.chunk.js"
+  },
+  {
+    "revision": "b7a99f20cfca00058cd8db8b16226dc1",
+    "url": "/static/js/2.b5928ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04521365ddb7600954f8",
+    "url": "/static/js/main.06bfdb60.chunk.js"
+  },
+  {
+    "revision": "1f20046d79d8828cb5be",
+    "url": "/static/js/runtime-main.bca5f4dc.js"
+  },
+  {
+    "revision": "ed7934a742f9f5c292a33717a7882ea4",
+    "url": "/static/media/background.ed7934a7.jpg"
+  },
+  {
+    "revision": "8c0da22c796e82c6119d498514874f19",
+    "url": "/static/media/backpack man.8c0da22c.jpg"
+  },
+  {
+    "revision": "616c4aa597a329851485399cb0242be3",
+    "url": "/static/media/bigLogo.616c4aa5.png"
+  },
+  {
+    "revision": "eecbf019e9ab700f60567d3cafb58e70",
+    "url": "/static/media/logo 1.eecbf019.png"
+  },
+  {
+    "revision": "7722aa97e334a83e2c57f0ba9efddcb6",
+    "url": "/static/media/mapbackground.7722aa97.png"
+  }
+]);
--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.9894cc48e4a38b62d318a3a27785696a.js"
+  "/precache-manifest.dff677e825ea79948e7e07078618bd3c.js"
 );
 
 self.addEventListener('message', (event) => {
